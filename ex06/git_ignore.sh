0014f1ec66531aa93acb4664a934bff95697ffd8
Faruk Kalyoncu <fkalyonc@k1m20s08.42kocaeli.com.tr>
2025-02-18 12:53:31 +0300

    06 bitti

---
+++ ex06/git_ignore.sh
@@ -0,0 +1,6 @@
+
+# Git deposundaki görmezden gelinen dosyaları listelemek için
+
+# .gitignore dosyasını kullanarak, görmezden gelinen dosyaları listele
+git ls-files --ignored --exclude-standard --others
+
